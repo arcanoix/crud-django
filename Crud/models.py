b0007cb5edb3bb7e4b945362fc597db3c5947a9e
Gustavo Herrera <gustavoh.2312@gmail.com>
2019-06-28 15:44:57 -0500

    ajustes return variable the post2

--- Crud/models.py
+++ Crud/models.py
@@ -5,5 +5,5 @@ class Post(models.Model):
 	title = models.CharField(max_length=120, help_text='title of message.')
 	message = models.TextField(help_text="what's on your mind ... ")
 
-	def _str_(self):
+	def __str__(self):
 		return self.title
